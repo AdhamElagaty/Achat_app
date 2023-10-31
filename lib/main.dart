4807e3d4771d3638fe3c591d844d90684917978f
Adham Elagaty <adhamalagaty@gmail.com>
2023-10-31 18:19:48 +0200

    Create project file

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AchatApp());
+}
+
+class AchatApp extends StatelessWidget {
+  const AchatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
